46d162a409c12ef7011ee911e05328509617a183
PicuBicu <championello@gmail.com>
2022-04-26 13:27:56 +0200

    Add serialized name annotation in currentweatherdata package

--- app/src/main/java/pl/piotrb/weatherapp/model/currentweatherdata/Main.java
+++ app/src/main/java/pl/piotrb/weatherapp/model/currentweatherdata/Main.java
@@ -1,5 +1,7 @@
 package pl.piotrb.weatherapp.model.currentweatherdata;
 
+import com.google.gson.annotations.SerializedName;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -9,8 +11,11 @@ import lombok.ToString;
 @Setter
 public class Main {
     private Double temp;
+    @SerializedName("feels_like")
     private Double feelsLike;
+    @SerializedName("temp_min")
     private Double tempMin;
+    @SerializedName("temp_max")
     private Double tempMax;
     private Integer pressure;
     private Integer humidity;
