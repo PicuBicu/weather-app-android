e57131a987c579aca25e829507e491c63d374280
PicuBicu <championello@gmail.com>
2022-05-08 22:50:49 +0200

    Fix additional fragment layout

--- app/src/main/java/pl/piotrb/weatherapp/fragment/AdditionalDataFragment.java
+++ app/src/main/java/pl/piotrb/weatherapp/fragment/AdditionalDataFragment.java
@@ -30,10 +30,10 @@ public class AdditionalDataFragment extends Fragment implements OnWeatherDataCha
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_additional_data, container, false);
-        windSpeedTextView = root.findViewById(R.id.wind_speed_text_view);
-        windDirectionTextView = root.findViewById(R.id.wind_direction_text_view);
-        humidityTextView = root.findViewById(R.id.humidity_text_view);
-        visibilityTextView = root.findViewById(R.id.visibility_text_view);
+        windSpeedTextView = root.findViewById(R.id.ad_wind_speed_text_view);
+        windDirectionTextView = root.findViewById(R.id.ad_wind_direction_text_view);
+        humidityTextView = root.findViewById(R.id.ad_humidity_text_view);
+        visibilityTextView = root.findViewById(R.id.ad_visibility_text_view);
         return root;
     }
 
@@ -47,10 +47,10 @@ public class AdditionalDataFragment extends Fragment implements OnWeatherDataCha
     @Override
     public void onWeatherDataChange() {
         viewModel.getWeatherData().observe(getViewLifecycleOwner(), weatherData -> {
-            val speedValue = weatherData.getWind().getSpeed() + "m/s";
-            val directionValue = weatherData.getWind().getDeg() + " degrees";
-            val humidityValue = weatherData.getMain().getHumidity() + "%";
-            val visibilityValue = weatherData.getVisibility() + " m";
+            val speedValue = weatherData.getWind().getSpeed() + "";
+            val directionValue = weatherData.getWind().getDeg() + "";
+            val humidityValue = weatherData.getMain().getHumidity() + "";
+            val visibilityValue = weatherData.getVisibility() + "";
             windSpeedTextView.setText(speedValue);
             windDirectionTextView.setText(directionValue);
             humidityTextView.setText(humidityValue);
